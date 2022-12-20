e757ccda03cffc0688363ed93146d6d9aad094e6
Alex Sander <alexsander01@hotmail.com.br>
2022-12-20 09:45:58 -0300

    feat: add validação hook form login

--- src/pages/login/login.pages.tsx
+++ src/pages/login/login.pages.tsx
@@ -3,8 +3,17 @@ import HeaderComponents from '../../components/header/header.components'
 import { LoginContainer, LoginContent, LoginHeadline, LoginInputContainer, LoginSubtitle } from './login.pages.styled'
 import { GiPizzaCutter, GiPizzaSlice } from 'react-icons/gi'
 import CustomInput from '../../components/custom-input/custom-input.components'
+import { useForm } from 'react-hook-form'
 
 const LoginPages = () => {
+  const { register, formState: { errors }, handleSubmit } = useForm()
+
+  const handleSubmitPress = (data: any) => {
+    console.log(data)
+  }
+
+  console.log(errors)
+
   return (
     <>
     <HeaderComponents/>
@@ -14,13 +23,20 @@ const LoginPages = () => {
         <CustomButom startIcon={<GiPizzaSlice size={25}/>}>Cadastrar</CustomButom>
             <LoginSubtitle>Ou Entre com a sua conta</LoginSubtitle>
         <LoginInputContainer>
-            <CustomInput placeholder='Digite seu E-mail' hasError={true}/>
+            <p>E-mail</p>
+            <CustomInput hasError={!!errors?.email}
+            placeholder='Digite seu E-mail' {...register('email',
+              { required: true })}/>
         </LoginInputContainer>
         <LoginInputContainer>
-            <CustomInput placeholder='Digite sua Senha' hasError={true}/>
+            <p>Senha</p>
+            <CustomInput hasError={!!errors?.password}
+            placeholder='Digite sua Senha' {...register('password',
+              { required: true })}/>
         </LoginInputContainer>
 
-        <CustomButom startIcon={<GiPizzaCutter size={25}/>}>Entrar</CustomButom>
+        <CustomButom startIcon={<GiPizzaCutter size={25}/>}
+         onClick={() => handleSubmit(handleSubmitPress)()}>Entrar</CustomButom>
         </LoginContent>
 
     </LoginContainer>
