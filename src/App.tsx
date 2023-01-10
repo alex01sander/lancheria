c7b13e9e1434a59d19eb2318551a8f41b93d469b
Alex Sander <alexsander01@hotmail.com.br>
2023-01-10 19:44:23 -0300

    feat: add User Context update onAuthStateChanged no login

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,35 @@
 import { onAuthStateChanged } from '@firebase/auth'
-import { FunctionComponent } from 'react'
+import { collection, getDocs, query, where } from '@firebase/firestore'
+import { FunctionComponent, useContext } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
-import { auth } from './config/firebase.config'
+import { auth, db } from './config/firebase.config'
+import { UserContext } from './contexts/user.contexts'
 import HomePages from './pages/home/home.pages'
 import LoginPages from './pages/login/login.pages'
 import SignUp from './pages/sign-up/sign-up.pages'
 
 const App: FunctionComponent = () => {
-  onAuthStateChanged(auth, (user) => {
-    console.log(user)
+  const { isAuthenticated, loginUser, logoutUser } = useContext(UserContext)
+
+  onAuthStateChanged(auth, async (user) => {
+    // se o usuario estiver logado no contexto, e o ususario do firebase (sign out)
+    // devemos limper o contexto
+
+    const isSigningOut = isAuthenticated && !user
+    if (isSigningOut) {
+      return logoutUser()
+    }
+
+    // se o usuario for  nulo no contexto, e não for nulo no firebase
+    // devemos fezer login
+    const isSigninIn = !isAuthenticated && user
+    if (isSigninIn) {
+      const querySnapshot = await getDocs(query(collection(db, 'users'), where('id', '==', 'user.uid')))
+
+      const userFromFirestore = querySnapshot.docs[0]?.data()
+      return loginUser(userFromFirestore as any)
+    }
   })
 
   return (
