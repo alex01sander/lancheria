05dbfd2145260238b4a9c7a92bf0228851acc0d9
Alex Sander <alexsander01@hotmail.com.br>
2023-02-05 15:16:21 -0300

    feat: add message to cart empty

--- src/components/cart/cart.components.tsx
+++ src/components/cart/cart.components.tsx
@@ -4,9 +4,18 @@ import { CartContainer, CartContent, CartEscapeArea, CartTitle, CartToggle, Cart
 import { BsCartCheck } from 'react-icons/bs'
 import { CartContext } from '../../contexts/cart.context'
 import CartItem from '../cart-item/cart-item.components'
+import { useNavigate } from 'react-router'
+import { TbShoppingCartPlus } from 'react-icons/tb'
 
 const Cart: FunctionComponent = () => {
-  const { isVisible, products, toggleCart, productsTotalPrice } = useContext(CartContext)
+  const { isVisible, products, toggleCart, productsTotalPrice, productCount } = useContext(CartContext)
+
+  const navigate = useNavigate()
+
+  const handleBuyCartClick = () => {
+    navigate('/')
+    toggleCart()
+  }
   return (
     <CartContainer isVisible={isVisible}>
         <CartContent>
@@ -16,8 +25,26 @@ const Cart: FunctionComponent = () => {
 
             {/* Produtos */}
             {products.map(product => <CartItem product={product} key={product.id}/>)}
+
+            {productCount > 0 && (
             <CartTotal>R$ {productsTotalPrice}</CartTotal>
+
+            )}
+
+            {productCount > 0 && (
             <CustomButom startIcon={<BsCartCheck/>}>Ir para o Checkout</CustomButom>
+            )}
+
+            {productCount === 0 && (
+              <p>Seu Carrinho está vazio</p>
+            )}
+
+            {productCount === 0 && (
+              <CustomButom
+              onClick={handleBuyCartClick}
+              startIcon={<TbShoppingCartPlus size={20}/>}
+              style={{ marginTop: 20 }}>Adicionar ao Carrinho</CustomButom>
+            )}
 
         </CartContent>
     </CartContainer>
